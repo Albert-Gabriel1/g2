6e7aeb9fe487c9e88e6c799f36a729c122e67bb4
Albert <albertgabriel834@gmail.com>
2025-12-21 01:30:45 -0300

    up1.1.1

--- js.js
+++ js.js
@@ -1,17 +1,32 @@
-let escuro = false
-function clicou(){
-    escuro = !escuro
-    if (escuro){
-    let sol = document.getElementById('modo-escuro')
-    document.body.style.backgroundColor = '#252424ff'
-    document.body.style.color = 'white'
-    sol.src= 'imagens/sol.png'
+    let escuro = false
+    function clicou(){
+        escuro = !escuro
+        if (escuro){
+        let sol = document.getElementById('modo-escuro')
+        document.body.style.backgroundColor = '#252424ff'
+        document.body.style.color = 'white'
+        sol.src= 'imagens/sol.png'
+        }
+        else{
+            let lua = document.getElementById('modo-escuro')
+        document.body.style.backgroundColor = 'white'
+        document.body.style.color = 'black'
+        lua.src = 'imagens/lua.png'
+        }    
     }
-    else{
-        let lua = document.getElementById('modo-escuro')
-    document.body.style.backgroundColor = 'white'
-    document.body.style.color = 'black'
-    lua.src = 'imagens/lua.png'
-    }    
-}
 
+    function clicou2(){
+        let nome = document.getElementById('nome').value;
+        let idade = document.getElementById('idade')
+        let senha = document.getElementById('senha')
+        let login = document.getElementById('login')
+        
+        if(senha.value.length <5){
+            alert('senha fraca')
+            return
+        }
+        localStorage.setItem('nome',nome)
+        let nome1 = localStorage.getItem('nome')
+            login.innerText =`Òla ${nome1}`
+            
+    }
