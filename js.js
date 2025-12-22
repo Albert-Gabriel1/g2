f2cd1d6bd3080e4b14a356c2d29daa1e91e72bd5
Albert <albertgabriel834@gmail.com>
2025-12-21 22:27:07 -0300

    up 1.1.2

--- js.js
+++ js.js
@@ -16,17 +16,50 @@
     }
 
     function clicou2(){
+        let hoje = new Date ()
+        let ano = hoje.getFullYear()
         let nome = document.getElementById('nome').value;
-        let idade = document.getElementById('idade')
-        let senha = document.getElementById('senha')
+        let idade1 = document.getElementById('idade').value;
+        let senha = document.getElementById('senha').value;
         let login = document.getElementById('login')
-        
-        if(senha.value.length <5){
-            alert('senha fraca')
-            return
-        }
+        let email = document.getElementById('email').value
+        let itemespecial = /[^a-zA-Z0-9]/;
+
         localStorage.setItem('nome',nome)
         let nome1 = localStorage.getItem('nome')
-            login.innerText =`Òla ${nome1}`
+            login.innerText =`Ola ${nome1} Seja bem vindo`
+        if(senha.length === 0 ){
+            alert('Coloque uma senha')
+            return;
+        }
+        else if(senha.length <5 || !itemespecial.test(senha))
+        {
+            alert('senha fraca')
+            return;
+        }
+        if(!nome1 || nome1.trim() === "")
+        {
+            alert('coloque algum nome')
+            return;
+        }
+        if (email === "" || !email.endsWith("@gmail.com") && !email.endsWith('@outlook.com')){
+            alert("Por Favor coloque um email o um valido")
+            return;
+        }
+        if(idade1 === ""){
+            alert('coloque alguma data')
+            return;
+        }
+        let idade = ano-idade1
+        if(idade <18 && idade >5)
+        {
+            alert(`Ola ${nome} você tem apenas ${idade} anos,peça permissão para seus pais para poder ver tudo com segurnaça`)
+            return;
             
-    }
+        }
+        if(idade <=0 || idade >=120){
+            alert('Coloque alguma idade valida')
+            return;
+        } 
+        
+}
